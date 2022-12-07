e9b36c1707deaad06944a80a4ff42ddd51f89bae
YangJiWon <jiwonhom@naver.com>
2022-12-07 22:22:48 +0900

    window 띄우기

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
-#include <iostream>
+//#include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** arg)
 {
-    std::cout<<"hello, opengl"<<std::endl;
+    //std::cout<<"hello, opengl"<<std::endl;
+    SPDLOG_INFO("start program");
+
+    //glfw 라이브러리 초기화, 실패하면 에러 출력후 종료
+    SPDLOG_INFO("Initialize glfw");
+    if(!glfwInit()){
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initilize glfw : {}", description);
+        return -1;
+
+    }
+
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+
+    if(!window)
+    {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("Start main loop");
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }      
